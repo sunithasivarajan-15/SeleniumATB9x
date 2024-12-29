38c9a7f1ccd2595e857571f4e57eafb1965553c4
sunithasivarajan-15 <sunithanagarajan23@gmail.com>
2024-12-29 22:50:49 +0530

    29 Dec

---
+++ src/test/java/org/example/test01.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class test01 {
+    @Test
+    public void test_selenium01(){
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+}
+}
